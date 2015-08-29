2fc714355aa061df83f401a2c811d80e83efadfc
hsieh jing <xqj198404@gmail.com>
2015-08-29 12:48:14 +0800

    1

---
+++ SmartDS/mds.UserService/Model/loginsessionModel.cs
@@ -0,0 +1,13 @@
+using System;
+ namespace mds.BaseModel
+ {
+ public class loginsession    { 
+       public int SessionId{            get;set;        }
+ public int LoginId{            get;set;        }
+ public string SessionSign{            get;set;        }
+ public DateTime CreateTime{            get;set;        }
+ public int Timelength{            get;set;        }
+ public string TimeUnit{            get;set;        }
+ 
+ }
+}
---
+++ SmartDS/mds.UserService/Model/loginuserModel.cs
@@ -0,0 +1,12 @@
+using System;
+ namespace mds.BaseModel
+ {
+ public class loginuser    { 
+       public int LoginId{            get;set;        }
+ public int UserId{            get;set;        }
+ public string LoginName{            get;set;        }
+ public string Pwd{            get;set;        }
+ public DateTime CreateTime{            get;set;        }
+ 
+ }
+}
